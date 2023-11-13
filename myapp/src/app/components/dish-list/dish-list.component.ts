c2da314cf8c520ab3394962b11cda9fdb6920bc8
Arjan <74960799+ArjanB96@users.noreply.github.com>
2023-11-14 00:12:15 +0100

    updated components

--- myapp/src/app/components/dish-list/dish-list.component.ts
+++ myapp/src/app/components/dish-list/dish-list.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../../models/dish.model';
 import { DishService } from '../../services/dish.service';
+import { ShoppingCartService } from '../../services/shopping-cart.service';  
 
 @Component({
-  selector: 'app-dish-list',
-  templateUrl: './dish-list.component.html'
-})
-export class DishListComponent implements OnInit {
-  dishes: Dish[];
-
-  constructor(private dishService: DishService) {}
-
-  ngOnInit(): void {
-    this.dishes = this.dishService.getDishes();
+    selector: 'app-dish-list',
+    templateUrl: './dish-list.component.html',
+  })
+  export class DishListComponent implements OnInit {
+    dishes: Dish[] = [];
+  
+    constructor(private dishService: DishService, private cartService: ShoppingCartService) {}
+  
+    ngOnInit(): void {
+      this.dishes = this.dishService.getDishes();
+    }
+  
+    addToCart(dish: Dish): void {
+      this.cartService.addToCart(dish);  // Roep de addToCart-methode aan in ShoppingCartService
+    }
   }
-}
--- myapp/src/app/components/shopping-cart/shopping-cart.component.ts
+++ myapp/src/app/components/shopping-cart/shopping-cart.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ShoppingCartService } from '../../services/shopping-cart.service';
+import { Dish } from '../../models/dish.model';  
 
 @Component({
   selector: 'app-shopping-cart',
   templateUrl: './shopping-cart.component.html'
 })
 export class ShoppingCartComponent implements OnInit {
-  items;
+  items: Dish[] = []; 
 
   constructor(private cartService: ShoppingCartService) {}
 
